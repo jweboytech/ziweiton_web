49cf3dc71486c84cc60a512f2e69d211a16fa7d0
jweboy <jweboy0630@gmail.com>
2025-03-14 11:33:58 +0800

    feat: icon 更新

--- layout/logo.tsx
+++ layout/logo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Logo = () => {
   return (
     <Image
-      src="https://ziweiton.oss-cn-hangzhou.aliyuncs.com/logo.jpg"
+      src="https://ziweiton.oss-cn-hangzhou.aliyuncs.com/logo.png"
       width={128}
       height={40}
       alt="logo"
